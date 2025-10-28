0a2b42ac88de389e343184493153d7eb0397f941
Dsalas8 <ssdamian1218@gmail.com>
2025-10-28 20:11:06 +0000

    edit

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -48,13 +48,20 @@ print(length_dec)
 # # 3. Indexing and Slicing
 # # ----------------------------------------
 chicago_mayor = "Johnson"
+#index slicing
+print(chicago_mayor[0:4])
+# the first number 0 is inclusive 
+#the last number is exclusive 
+print(chicago_mayor[4:])
+print(chicago_mayor[2:-1])
+print(chicago_mayor[0:5])
 print(len(chicago_mayor))
 print(chicago_mayor[0])
 print(chicago_mayor[4])
 print(chicago_mayor[-1])
 # # Indexing: Access characters by position (0-based index)
-# print("First character:", phrase[0])  # Output: P
-# print("Last character:", phrase[-1])  # Output: !
+print("First character:", phrase[0])  # Output: P
+print("Last character:", phrase[-1])  # Output: !
 
 # # Slicing: Get a range of characters (start inclusive, end exclusive)
 # print("Characters 1 to 4:", phrase[1:4])  # Output: yth
